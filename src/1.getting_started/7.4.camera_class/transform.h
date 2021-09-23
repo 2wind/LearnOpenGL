2e4f89ba32677394d84153552dff25d10e40a350
Kim Yongwook <rladyddnrl@gmail.com>
2021-09-23 14:44:29 +0900

    added shear manipulation

--- src/1.getting_started/7.4.camera_class/transform.h
+++ src/1.getting_started/7.4.camera_class/transform.h
@@ -16,16 +16,25 @@ class Transform{
         glm::vec3 scale;
         glm::vec3 pivot;
 
+        glm::vec2 shear_x;
+        glm::vec2 shear_y;
+        glm::vec2 shear_z;
+
         Transform(){
             parent = NULL;
             position = glm::vec3(0.0f);
             rotation = glm::quat(glm::vec3(0.0f));
             scale = glm::vec3(1.0f, 1.0f, 1.0f);
             pivot = glm::vec3(0.0f);
+
+            shear_x = glm::vec2(0.0f);
+            shear_y = glm::vec2(0.0f);
+            shear_z = glm::vec2(0.0f);
         }
+
                 
         glm::mat4 GetMatrix(){
-            glm::mat4 S = glm::scale(glm::mat4(1.0f), scale);
+            glm::mat4 S = GetShearMatrix() * glm::scale(glm::mat4(1.0f), scale);
             glm::mat4 RS = glm::translate(glm::toMat4(rotation) * glm::translate(S, -pivot), pivot);
             glm::mat4 TRS = glm::translate(RS, position - pivot);
 
@@ -44,7 +53,17 @@ class Transform{
             return glm::translate(GetRawTranslation(), -pivot);
         }
 
-
+    private:
+        glm::mat4 GetShearMatrix(){
+            glm::mat4 matrix = glm::mat4(1.0f);
+            matrix[0][1] = shear_x[0];
+            matrix[0][2] = shear_x[1];
+            matrix[1][0] = shear_y[0];
+            matrix[1][2] = shear_y[1];
+            matrix[2][0] = shear_z[0];
+            matrix[2][1] = shear_z[1];
+            return matrix;
+        }
 };
 
 glm::mat4 get_world_matrix(Transform transform){
